cd1286325d2b5dec86735abf37265e14600423b0
ThummarDarshan <darshantce.059@gmail.com>
2025-05-08 08:08:34 +0530

    Update oop-1.2-object.cpp

--- oop-1.2-object.cpp
+++ oop-1.2-object.cpp
@@ -1,6 +1,4 @@
 
-
-
 /*Write a program for an inventory management system using both procedural and
 object-oriented approaches. The system should handle products with attributes:
 ID, name, quantity, and price.
